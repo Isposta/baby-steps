9efcc8d3f043a2adb68725d97d0e652fc306540e
AlpGozukucuk@gmail.com <AlpGozukucuk@gmail.com>
2024-03-07 16:58:07 +0300

    basic props

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,10 +1,36 @@
 import './App.css';
 import { Greet } from './components/Greet'
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Big',
+    last: 'Boss',
+  }
+
+const nameList = [
+  {
+    first: 'Big',
+    last: 'Boss',
+  },
+  {
+    first: 'Venom',
+    last: 'Snake',
+  },
+  {
+    first: 'Kazuhara',
+    last: 'Miller'
+  }
+]
+
+
+
   return (
     <div className="App">
-     <Greet name='Ispost' />
+     <Greet name='Ispost' messageCount={20} isLoggedIn={false} />
+     <Person name={personName} />
+     <PersonList names={nameList} />
     </div>
   );
 }
