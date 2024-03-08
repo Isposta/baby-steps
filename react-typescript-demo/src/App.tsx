32daf23555ea1bab9d68d4c357394e5deb05338f
AlpGozukucuk@gmail.com <AlpGozukucuk@gmail.com>
2024-03-08 18:38:55 +0300

    generic props

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css';
-import { Private } from './components/auth/Private';
-import { Profile } from './components/auth/Profile';
+
 
 
 
 function App() {
   return( 
   <div className='App'>
- <Private isLoggedIn={true} component={Profile} />
+
   </div>
   )
 }
