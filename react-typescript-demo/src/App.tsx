344014caa5255bb8991911795758c70d8f23375a
AlpGozukucuk@gmail.com <AlpGozukucuk@gmail.com>
2024-03-08 05:28:46 +0300

    event props

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -3,6 +3,9 @@ import { Heading } from './components/Heading';
 import { Oscar } from './components/Oscar';
 import { Status } from './components/Status'
 import { Greet } from './components/Greet'
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+
 
 function App() {
   return( 
@@ -13,6 +16,10 @@ function App() {
       <Heading>Oscar goes to Leonardo DiCaprio</Heading>
     </Oscar>
     <Greet name= 'Ispost' isLoggedIn={false}/>
+    <Button handleClick={(event, id) =>{
+      console.log('Button clicked', event, id)
+    }}/>
+    <Input value= '' handleChange={event => console.log(event)} />
   </div>
   )
 }
